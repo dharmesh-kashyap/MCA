74c62f91113d53f0649e79ea6f6e4924b7f89168
dharmesh-kashyap <dharmeshkashyap46@gmail.com>
2024-01-10 20:08:14 +0530

    updated

--- MCA Semester 2/OOPS-2 JAVA/Java Lab/Week1/InvestmentPlan.java
+++ MCA Semester 2/OOPS-2 JAVA/Java Lab/Week1/InvestmentPlan.java
@@ -3,7 +3,7 @@ import java.lang.Math;
 
 public class InvestmentPlan {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float loanAmt, timePeriod, monthlyPayment, TotalPayment, monthlyInterest;
         System.out.print("Enter the Loan Amount you want to borrow: ");
@@ -11,13 +11,13 @@ public class InvestmentPlan {
         System.out.print("Enter the Time Period: ");
         timePeriod = sc.nextFloat();
         System.out.println("Interest rate\t\tMonthly Payment\tTotal Payment");
-        for(float i = 5; i <= 8 ; i += (1/8f)){
-            monthlyInterest =  i / 12f;
-            float temp2 = (float) Math.pow(( 1f + monthlyInterest), (timePeriod * 12f) );
+        for (float i = 5; i <= 8; i += (1 / 8f)) {
+            monthlyInterest = (i / 100) / 12f;  
+            float temp2 = (float) Math.pow((1f + monthlyInterest), (timePeriod * 12f));
             float temp = 1f - (1f / temp2);
-            monthlyPayment = (loanAmt * monthlyInterest)/temp;
+            monthlyPayment = (loanAmt * monthlyInterest) / temp;
             TotalPayment = monthlyPayment * timePeriod * 12;
-            System.out.println( i + "%" +"\t\t\t\t" +monthlyPayment+"\t\t\t"+TotalPayment );
+            System.out.printf("%.2f%%\t\t\t\t%.2f\t\t\t%.2f\n", i, monthlyPayment, TotalPayment);
         }
     }
 }
