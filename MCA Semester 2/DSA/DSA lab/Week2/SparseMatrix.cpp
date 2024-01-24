6ae08305b40f72ffb80b59ecc01fa377965beb67
dharmesh-kashyap <dharmeshkashyap46@gmail.com>
2024-01-24 08:14:13 +0530

    sparse update

--- MCA Semester 2/DSA/DSA lab/Week2/SparseMatrix.cpp
+++ MCA Semester 2/DSA/DSA lab/Week2/SparseMatrix.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 using namespace std;
+struct array1 {  
+    int arr[100];
+};
 
 int main() {
     int row, column, size = 0; // Initialize size to 0
@@ -13,15 +16,18 @@ int main() {
     // Input matrix elements
     cout << "Enter matrix elements:" << endl;
     for(int i = 0; i < row; i++) {
-        for(int j = 0; j < column; j++)
+        for(int j = 0; j < column; j++){
+            cout<<"Enter the Element on "<<i<<" "<<j<<": ";
             cin >> M[i][j];
+        }
     }
 
     // Display the entered matrix
     cout << "Entered matrix:" << endl;
     for(int i = 0; i < row; i++) {
-        for(int j = 0; j < column; j++) 
+        for(int j = 0; j < column; j++) {
             cout << M[i][j] << " ";
+        }
         cout << endl;
     }
 
@@ -35,31 +41,49 @@ int main() {
 
     int threshold = 2.0 / 3 * (row * column);
 
-    if (size <= threshold) {
-        cout << "Sparse matrix" << endl;
-        int sp_mat[size][3], x = 0;
+    array1 a,b,c;
+    
+    int k=0;
+
+    if (size < threshold) {
+        cout << "\nIt is a Sparse matrix" << endl;
 
         // Create 1D sparse matrix representation
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < column; j++) {
                 if(M[i][j] != 0) {
-                    sp_mat[x][0] = i;
-                    sp_mat[x][1] = j;
-                    sp_mat[x][2] = M[i][j];
-                    x++;
+                   a.arr[k] = i;
+                   b.arr[k] = j;
+                   c.arr[k] = M[i][j];
+                   k++;
                 }
             }
         }
 
+        cout<<"The given matrix in the form of an array of triplets: \n";
+        cout<<row<<"\t"<<column<<"\t"<<size;
+         for(int i = 0; i < size; i++){
+            cout<<"\n"<<a.arr[i]<<"\t"<<b.arr[i]<<"\t"<<c.arr[i];
+         }
+
         // Display 1D sparse matrix representation
-        cout << "1D sparse matrix representation:" << endl;
-        for(int i = 0; i < size; i++)
-            cout << sp_mat[i][2] << " ";
+        cout << "\n1D sparse matrix representation:" << endl;
+        for(int i = 0; i < size; i++){
+            cout << c.arr[i] << " ";
+        }
+
+        int newmat[row][column];
+
+        for(int i = 0; i < row; i++) {
+            for(int j = 0; j < column; j++){
+                newmat[i][j]=0;
+            }
+        }
 
         // Reconstruct the matrix
         cout << "\nReconstructed matrix:" << endl;
         for(int i = 0; i < size; i++)
-            M[sp_mat[i][0]][sp_mat[i][1]] = sp_mat[i][2];
+            newmat[a.arr[i]][b.arr[i]] = c.arr[i];
 
         for(int i = 0; i < row; i++) {
             for(int j = 0; j < column; j++)
