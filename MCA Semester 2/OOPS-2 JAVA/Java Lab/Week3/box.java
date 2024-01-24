a9ce2b7c49317640f5066c2d93ecc93953a7e023
dharmesh-kashyap <dharmeshkashyap46@gmail.com>
2024-01-24 23:08:05 +0530

    updated

--- MCA Semester 2/OOPS-2 JAVA/Java Lab/Week3/box.java
+++ MCA Semester 2/OOPS-2 JAVA/Java Lab/Week3/box.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 public class box{
     private static Scanner sc = new Scanner(System.in);
-    protected int weight;
+    protected int width;
     protected int height;
     protected int depth;
     protected int volume;
 
 
     public box(int a,int b ,int c){
-        weight = a;
+        width = a;
         height = b;
         depth = c;
     }
 
     public void display(){
 
-		volume = weight*height*depth;
+		volume = width*height*depth;
         System.out.println("Product details: ");
-        System.out.println("Weight: " + weight);
+        System.out.println("Width: " + width);
         System.out.println("Heigh: " + height);
         System.out.println("depth: " + depth);
         System.out.println("Volume: " + volume);
@@ -28,13 +28,13 @@ public class box{
     public static void main(String[] args){
 
         System.out.print("Enter Box Dimensions: \n");
-        System.out.print("Weight: ");
-        int weight = sc.nextInt();
+        System.out.print("Width: ");
+        int width = sc.nextInt();
         System.out.print("Height: ");
         int height = sc.nextInt();
         System.out.print("Depth: ");
         int depth = sc.nextInt();
-        box aa = new box(weight,height,depth);
+        box aa = new box(width,height,depth);
         aa.display();
 
         sc.close();
