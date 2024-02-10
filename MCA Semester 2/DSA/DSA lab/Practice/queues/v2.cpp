48379fd6fb5a728bef991d6c49ed754b8921dab8
dharmesh-kashyap <dharmeshkashyap46@gmail.com>
2024-02-11 01:01:08 +0530

    v2

---
+++ MCA Semester 2/DSA/DSA lab/Practice/queues/v2.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+
+class Queue
+{
+    int *arr, front, rear, capacity;
+
+public:
+    Queue(int cap)
+    {
+        capacity = cap;
+        front = -1;
+        rear = -1;
+        arr = new int[capacity];
+    }
+
+    ~Queue()
+    {
+        delete[] arr;
+    }
+
+    bool isEmpty()
+    {
+        return front == -1;
+    }
+
+    bool isFull()
+    {
+        return (rear +1) % capacity == front;
+    }
+
+    void push(int x)
+    {
+        if (isFull())
+        {
+            cout << "The Queue is full\n";
+            return;
+        }
+        else
+        {
+            if (isEmpty())
+            {
+                front = 0;
+            }
+            rear = (rear+1)%capacity;
+            arr[rear] = x;
+        }
+    }
+
+    int pop()
+    {
+        if (isEmpty())
+        {
+            cout << "The queue is empty\n";
+            return 0;
+        }
+        int temp = arr[front];
+        front = (front + 1) % capacity;
+        return temp;
+    }
+
+   void DisplayQ()
+    {
+        if (isEmpty())
+        {
+            cout << "The Queue is empty\n";
+            return;
+        }
+        cout << "The Queue is :\n";
+        int i = front;
+        do
+        {
+            cout << arr[i] << " ";
+            i = (i + 1) % capacity;
+        } while (i != (rear + 1) % capacity);
+        cout << endl;
+    }
+
+    void display()
+    {
+        if (isEmpty())
+        {
+            cout << "The Queue is empty\n";
+            return;
+        }
+        cout << "\nThe value at front is: " << arr[front];
+        cout << "\nThe value at rear is: " << arr[rear]<<endl;
+    }
+};
+
+int main()
+{
+    int m, choice;
+
+    cout << "Enter the Capacity: ";
+    cin >> m;
+
+    Queue Q(m);
+
+do
+{
+    cout<<"----------Menu----------\n1. Push elements\n2. Pop element\n3. Display the queue\n4. Display The front and Rear\n5. Exit\n";
+    cout<<"Enter the choice: ";
+    cin>>choice;
+
+    switch(choice){
+        case 1:
+        int j;
+        cout<<"Enter the element: ";
+        cin>>j;
+        Q.push(j);
+        break;
+
+        case 2: 
+        cout<<"The Popped element is: "<<Q.pop()<<endl;
+        break;
+
+        case 3: 
+        Q.DisplayQ();
+        break;
+
+        case 4:
+        Q.display();
+        break;
+
+        case 5:
+        cout<<"Exiting the Loop";
+        break;
+
+        default: 
+        cout<<"bakchod ho kya tum\n";
+        break;
+    }
+    
+} while (choice!=5);
+
+    
+   
+    return 0;
+}
