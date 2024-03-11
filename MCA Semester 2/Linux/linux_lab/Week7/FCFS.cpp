98b776454af3d721aece642daf6966c565ba65fe
dharmesh-kashyap <dharmeshkashyap46@gmail.com>
2024-03-12 01:44:45 +0530

    Linux week7

---
+++ MCA Semester 2/Linux/linux_lab/Week7/FCFS.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the no. of Process: ";
+    cin >> n;
+    int p[n];
+    int bt[n];
+    int at[n];
+    int wt[n];
+    int tat[n];
+    int ct[n];
+
+    int pi = 0;
+    p[0] = 0;
+    int temp;
+
+    cout << "Enter the  Process number: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "The process no: ";
+        p[i] = pi++;
+        cout << p[i] << endl;
+        cout << "Enter the Arrival time: ";
+        cin >> at[i];
+        cout << "Enter the Burst time: ";
+        cin >> bt[i];
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+
+            if (at[i] < at[j])
+            {
+                int temp = at[j];
+                at[j] = at[i];
+                at[i] = temp;
+
+                int temp2 = bt[j];
+                bt[j] = bt[i];
+                bt[i] = temp2;
+
+                int temp3 = p[j];
+                p[j] = p[i];
+                p[i] = temp3;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            ct[i] = bt[i] + at[i];
+        }
+        else
+        {
+            temp = 0;
+            if (ct[i - 1] < at[i])
+            {
+                temp = at[i] - ct[i - 1];
+            }
+            ct[i] = bt[i] + ct[i - 1] + temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        tat[i] = ct[i] - at[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        wt[i] = tat[i] - bt[i];
+    }
+    cout << "Entered details: " << endl;
+    cout << "|   PID  \t|\t   Arrival   \t|\t   burst   \t|\t  Completion  \t|\t  TAT  \t|\t  waiting  \t|\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << "\t" << p[i] << "\t\t";
+        cout << "\t" << at[i] << "\t\t";
+        cout << "\t" << bt[i] << "\t\t";
+        cout << "\t" << ct[i] << "\t\t";
+        cout << "\t" << tat[i] << "\t\t";
+        cout << "\t" << wt[i] << "\t\t";
+        cout << endl;
+    }
+
+    float totalwt = 0;
+    float totaltat = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        totalwt += wt[i];
+        totaltat += tat[i];
+    }
+
+    cout << "avg waiting time: " << totalwt / n << endl;
+    cout << "avg turnaround time: " << totaltat / n << endl;
+
+    return 0;
+}
---
+++ MCA Semester 2/Linux/linux_lab/Week7/SJF.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of Processes: ";
+    cin >> n;
+
+    int p[n]; 
+    int bt[n]; 
+    int at[n]; 
+    int wt[n]; 
+    int tat[n]; 
+    int ct[n]; 
+
+    cout << "Enter the Process details:\n";
+    for (int i = 0; i < n; i++) {
+        cout << "Enter the Arrival time for Process " << i + 1 << ": ";
+        cin >> at[i];
+        cout << "Enter the Burst time for Process " << i + 1 << ": ";
+        cin >> bt[i];
+        p[i] = i + 1; 
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (at[j] > at[j + 1]) {
+                int temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
+                
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+                
+                temp = p[j];
+                p[j] = p[j + 1];
+                p[j + 1] = temp;
+            }
+        }
+    }
+
+    int currentTime = 0;
+for (int i = 0; i < n; i++) {
+    if (currentTime < at[i]) {
+        currentTime = at[i]; 
+    }
+    currentTime += bt[i]; 
+    ct[i] = currentTime; 
+}
+
+
+    for (int i = 0; i < n; i++) {
+        tat[i] = ct[i] - at[i];
+        wt[i] = tat[i] - bt[i];
+    }
+
+    cout << "PID\tArrival\tBurst\tCompletion\tTurnaround\tWaiting\n";
+    for (int i = 0; i < n; i++) {
+        cout << p[i] << "\t" << at[i] << "\t" << bt[i] << "\t" << ct[i] << "\t\t" << tat[i] << "\t\t" << wt[i] << endl;
+    }
+
+    float avgWaitingTime = 0;
+    float avgTurnaroundTime = 0;
+    for (int i = 0; i < n; i++) {
+        avgWaitingTime += wt[i];
+        avgTurnaroundTime += tat[i];
+    }
+    avgWaitingTime /= n;
+    avgTurnaroundTime /= n;
+
+    cout << "Average Waiting Time: " << avgWaitingTime << endl;
+    cout << "Average Turnaround Time: " << avgTurnaroundTime << endl;
+
+    return 0;
+}
